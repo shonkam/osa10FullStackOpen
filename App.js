9d002ffe02995070efe523b2db373db7146935ab
shonkam <samu.honkamaki@gmail.com>
2021-11-01 11:46:32 +0200

    10.3

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+import RepositoryList from './src/components/RepositoryList';
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <RepositoryList />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RepositoryItem = ({
+  fullName,
+  description,
+  language,
+  forksCount,
+  stargazersCount,
+  ratingAverage,
+  reviewCount
+}) => {
+
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Average rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
