b515f8a25d5bc19224e9e529e7b2797ca81d0b88
shonkam <samu.honkamaki@gmail.com>
2021-11-19 13:43:41 +0200

    10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
+import { Button, Menu, Provider } from 'react-native-paper';
 
 
 const styles = StyleSheet.create({
@@ -33,20 +34,41 @@ const renderItem = ({ item }) => {
 
 const RepositoryList = () => {
 
-  const { repositories } = useRepositories();
+  const [visible, setVisible] = useState(false);
+  const [sortedBy, setSortedBy] = useState('Latest repositories');
+  const { repositories } = useRepositories(sortedBy);
 
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
 
+  const openMenu = () => setVisible(true);
 
+  const closeMenu = () => setVisible(false);
 
   return (
+    <Provider>
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={< Button
+          onPress={openMenu}
+          mode="contained"
+        >
+          {sortedBy}
+        </Button>}>
+        <Menu.Item onPress={() => { setSortedBy('Latest repositories'), closeMenu(); }} title="Latest repositories" />
+        <Menu.Item onPress={() => { setSortedBy('Highest rated repositories'), closeMenu(); }} title="Highest rated repositories" />
+        <Menu.Item onPress={() => { setSortedBy('Lowest rated repositories'), closeMenu(); }} title="Lowest rated repositories" />
+      </Menu >
+
       <FlatList
       data={repositoryNodes}
       renderItem={renderItem}
       ItemSeparatorComponent={ItemSeparator}
     />
+
+    </Provider>
   );
 };
 
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,21 +1,38 @@
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
+const useRepositories = (sortedBy) => {
+
+  let orderBy;
+  let orderDirection;
+
+  if (sortedBy === 'Latest repositories') {
+    orderBy = 'CREATED_AT';
+  }
+  if (sortedBy === 'Highest rated repositories') {
+    orderBy = 'RATING_AVERAGE';
+    orderDirection = 'DESC';
+  }
+  if (sortedBy === 'Lowest rated repositories') {
+    orderBy = 'RATING_AVERAGE';
+    orderDirection = 'ASC';
+  }
 
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
-    fetchPolicy: 'cache-and-network'
+    variables: { orderBy, orderDirection },
+    fetchPolicy: 'cache-and-network',
   });
-
-  while (loading === true) {
+  while (loading) {
     return [];
   }
 
   if (error === true) {
     return error;
   }
-
-  return data;
+  if (data) {
+    return data;
+  }
 };
+
 
 export default useRepositories;
